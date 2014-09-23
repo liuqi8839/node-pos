639549d0f42f2a58230610acdcfc803a2cb4430f
liuqi8839 <liuqitiger@qq.com>
2014-09-23 20:13:28 +0800

    show cart

--- public/viewStage/item_list.js
+++ public/viewStage/item_list.js
@@ -2,3 +2,20 @@
 $(function(){
     $('#item-list').addClass('active');
 });
+
+$('.addGood').click(function(){
+    var temp = [];
+    $($(this).parent().siblings()).each(function(){
+        temp.push($(this).text());
+    });
+    var good = {
+        kind:temp[0],
+        name:temp[1],
+        price:temp[2],
+        unit:temp[3],
+        twosendone:temp[4]
+    };
+    $.post('/addGood',{good:good},function(data){
+        $('#cart').find('#item-numbers').text(data);
+    });
+});
--- routes/index.js
+++ routes/index.js
@@ -27,28 +27,53 @@ module.exports = function(app) {
             });
         });
     });
-    app.post('/item_list', function (req, res) {
-    });
     app.get('/cart', function (req, res) {
-        res.render('cart', { title: ' 购物车' });
-    });
-    app.post('/cart', function (req, res) {
+        var goods = req.session.cart;
+        var gives = [];
+        _.each(goods,function(good){
+            if(good.twosendone == "true" && good.num > 2){
+                var give = _.clone(good);
+                give.num = parseInt(give.num/3);
+                gives.push(give);
+            }
+        });
+        res.render('cart',{
+            title:"购物清单",
+            total:req.session.total,
+            goods:goods,
+            gives:gives
+        });
     });
     app.get('/pay', function (req, res) {
-        res.render('pay', { title: ' 付款' });
+        var goods = req.session.cart;
+        var gives = [];
+        _.each(goods,function(good){
+            if(good.twosendone == "true" && good.num > 2){
+                var give = _.clone(good);
+                give.num = parseInt(give.num/3);
+                gives.push(give);
+            }
+        });
+        res.render('pay',{
+            title:"付款",
+            total:req.session.total,
+            goods:goods,
+            gives:gives
+        });
     });
     app.post('/pay', function (req, res) {
     });
 
-    app.post('/addGood',function(req,res){
+    app.post('/addGood', function(req,res){
         var good = req.body.good;
         var cart = req.session.cart;
         var hadGood = _.findWhere(cart,{name:good.name});
         if(hadGood){
-            good.num = hadShop.num + 1;
+            good.num = hadGood.num + 1;
             var index = _.indexOf(cart,hadGood);
             cart[index] = good;
-        }else{
+        }
+        else{
             good.num = 1;
             cart.push(good);
         }
@@ -58,5 +83,34 @@ module.exports = function(app) {
         res.writeHead(200,{'Content-type':'text/plain'});
         res.write(total + "");
         res.end();
+    });
+    app.get('/subCart',function(req,res){
+        var goods = req.session.cart;
+        var good = _.findWhere(goods,{name:req.query.goodName});
+        var index = _.indexOf(goods,good);
+        good.num = good.num - 1;
+        if(good.num == 0){
+            goods.splice(index,1);
+        }else{
+            goods[index] = good;
+        }
+        req.session.total = req.session.total - 1;
+        req.session.cart = goods;
+
+        if(req.session.total == 0){
+            res.redirect('/item_list');
+        }else{
+            res.redirect('/cart');
+        }
+    });
+    app.get('/addCart',function(req,res){
+        var goods = req.session.cart;
+        var good = _.findWhere(goods,{name:req.query.goodName});
+        var index = _.indexOf(goods,good);
+        good.num = good.num + 1;
+        goods[index] = good;
+        req.session.total = req.session.total + 1;
+        req.session.cart = goods;
+        res.redirect('/cart');
     });
 };
